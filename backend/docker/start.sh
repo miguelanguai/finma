2332c3cd9707ec9d4d93a49928c508aa4d134b47
Miguel Angel <miguelangelguaivi@gmail.com>
2025-09-12 16:37:11 +0200

    feat: first commit

---
+++ backend/docker/start.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Activar el entorno de Conda
+source /opt/conda/bin/activate finma
+
+# Ejemplo para una imagen basada en python:3.12-slim
+RUN apt-get update && apt-get install -y postgresql-server-dev-all
+
+# Esperar a que la base de datos esté lista
+echo "Esperando a que PostgreSQL inicie..."
+until nc -z -v -w30 postgis 5432
+do
+  echo "Esperando a la base de datos..."
+  sleep 1
+done
+echo "PostgreSQL está disponible."
+
+# Aplicar migraciones
+echo "Aplicando migraciones..."
+python manage.py migrate
+
+# Iniciar Gunicorn para servir Django
+echo "Iniciando el servidor..."
+exec gunicorn backend.wsgi:application --bind 0.0.0.0:8000 --access-logfile - --error-logfile -
---
+++ backend/objetivo/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ObjetivoConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'objetivo'
