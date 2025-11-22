6ebcfccb1f489ace364bebd502c519784956266a
Miguel Angel <miguelangelguaivi@gmail.com>
2025-11-22 10:41:23 +0100

    feat: crear mapeo de periodos y categorias

--- backend/periodo/models.py
+++ backend/periodo/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 
-# Create your models here.
 class Periodo(models.Model):
     """Clase Período
 
@@ -19,4 +18,34 @@ class Periodo(models.Model):
     def __str__(self):
         return f"""
         periodo {self.nombre} en fecha {self.fecha}.
+        """
+
+
+class MapPeriodoCategoria(models.Model):
+    """Clase MapPeriodoCategoria
+
+    Es el mapeo entre entidades Periodo y Categoria
+
+    porc_ideal_fijo (float): porcentaje ideal respecto a fijo de este período. Ejemplo: 12.725
+    porc_ideal_estimado (float): porcentaje ideal respecto a estimado de este período.
+    Ejemplo: 12.725
+    porc_ideal_obtenido (float): porcentaje ideal respecto a obtenido de este período.
+    Ejemplo: 12.725
+
+    Args:
+        models (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+
+    porc_ideal_fijo = models.DecimalField(max_digits=5, decimal_places=3, null=True)
+    porc_ideal_estimado = models.DecimalField(max_digits=5, decimal_places=3, null=True)
+    porc_ideal_obtenido = models.DecimalField(max_digits=5, decimal_places=3, null=True)
+
+    def __str__(self):
+        return f"""
+        porcentaje ideal respecto a fijo: {self.porc_ideal_fijo}
+        porcentaje ideal respecto a estimado: {self.porc_ideal_estimado}
+        porcentaje ideal respecto a obtenido: {self.porc_ideal_obtenido}
         """
