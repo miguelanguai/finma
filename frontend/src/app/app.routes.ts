5eb6e45940f5dcb60c4e986ae5d0f8c6554ac6c6
Miguel Angel <miguelangelguaivi@gmail.com>
2025-12-16 17:30:27 +0100

    build: :fire: retirar angular v17ç

--- frontend/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { PeriodoListComponent } from './periodo/periodo-list/periodo-list.component';
-
-export const routes: Routes = [
-    {
-        path: '',
-        component: PeriodoListComponent
-    }
-];
