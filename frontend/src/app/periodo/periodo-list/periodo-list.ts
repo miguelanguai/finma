65c4177633dac6d268a8658d94c3f5b11406cc01
Miguel Angel <miguelangelguaivi@gmail.com>
2025-12-16 17:42:04 +0100

    feat: crear modulo, componente y servicio de periodo

---
+++ frontend/src/app/periodo/periodo-list/periodo-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-periodo-list',
+  imports: [],
+  templateUrl: './periodo-list.html',
+  styleUrl: './periodo-list.css',
+})
+export class PeriodoList {
+
+}
