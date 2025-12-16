39a4a085f26b38b8802643a5858e32dccc772bed
Miguel Angel <miguelangelguaivi@gmail.com>
2025-12-16 18:31:00 +0100

    refactor: refactorizar rutas
    poner landing page en raiz y periodo con periodo

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
+import { LandingPage } from './home/landing-page/landing-page';
 import { PeriodoList } from './periodo/periodo-list/periodo-list';
 
 export const routes: Routes = [
     {
         path: '',
+        component: LandingPage
+    },
+    {
+        path: 'periodo',
         component: PeriodoList
-    }
+    },
 ];
