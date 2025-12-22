58d37069d2fb0cc642bfa1104a644552d2e3ea90
Miguel Angel <miguelangelguaivi@gmail.com>
2025-12-22 20:34:51 +0100

    refactor: retirar variables como privadas y dejarlas publicas
    en Categoria

--- frontend/src/app/categoria/Categoria.ts
+++ frontend/src/app/categoria/Categoria.ts
@@ -1,8 +1,8 @@
 export class Categoria {
-    private id: number | null;
-    private nombre: string;
-    private is_gasto: boolean;
-    private padre: Categoria | null;
+    id: number | null;
+    nombre: string;
+    is_gasto: boolean;
+    padre: Categoria | null;
 
     constructor();
     constructor(id?: number | null, nombre?: string, is_gasto?: boolean, padre?: Categoria);
@@ -13,37 +13,5 @@ export class Categoria {
         this.padre = padre ?? null
     }
 
-    // Getters & Setters
-
-    public getId(): number | null {
-        return this.id;
-    }
-
-    public getNombre(): string {
-        return this.nombre;
-    }
-
-    public getIsGasto(): boolean {
-        return this.is_gasto;
-    }
-
-    public getPadre(): Categoria | null {
-        return this.padre;
-    }
-
-    public setId(id: number): void {
-        this.id = id;
-    }
-
-    public setNombre(nombre: string): void {
-        this.nombre = nombre;
-    }
-
-    public setIsGasto(is_gasto: boolean): void {
-        this.is_gasto = is_gasto;
-    }
-
-    public setPadre(padre: Categoria): void {
-        this.padre = padre;
-    }
+    
 }
