3562f0ab85cf132972eac204a8775c43c334fff6
Miguel Angel <miguelangelguaivi@gmail.com>
2025-12-23 12:33:09 +0100

    refactor: cambiar Categoria para tener como referencia otra Categoria en el atributo padre

--- frontend/src/app/categoria/Categoria.ts
+++ frontend/src/app/categoria/Categoria.ts
@@ -2,16 +2,16 @@ export class Categoria {
     id: number | null;
     nombre: string;
     is_gasto: boolean;
-    padre: number | null;
+    padre: Categoria | null;
 
     constructor();
-    constructor(id?: number | null, nombre?: string, is_gasto?: boolean, padre?: number);
-    constructor(id?: number | null, nombre?: string, is_gasto?: boolean, padre?: number) {
+    constructor(id?: number | null, nombre?: string, is_gasto?: boolean, padre?: Categoria);
+    constructor(id?: number | null, nombre?: string, is_gasto?: boolean, padre?: Categoria) {
         this.id = id ?? null;
         this.nombre = nombre ?? '';
         this.is_gasto = is_gasto ?? true;
         this.padre = padre ?? null
     }
 
-    
+
 }
